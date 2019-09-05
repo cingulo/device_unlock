c2c0e21438559f718d390d0a23b497672b2d24af
Github <rzdgodoy@gmail.com>
2019-09-05 11:04:21 +0700

    added needed usage description for face id in the example app; added unlock funciontality using iOS LocalAuthentication framework;

--- ios/Classes/SwiftDeviceUnlockPlugin.swift
+++ ios/Classes/SwiftDeviceUnlockPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import LocalAuthentication
 
 public class SwiftDeviceUnlockPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -11,16 +12,36 @@ public class SwiftDeviceUnlockPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
       case "request":
-        let unlocked = self.request()
-        result(unlocked)
+        let unlocked = self.request { (unlocked) in
+          result(unlocked)
+        }
         break
       default:
         result(FlutterMethodNotImplemented)
     }
   }
 
-  private func request() -> Bool {
-    return true
+  private func request(completionBlock: @escaping (Bool) -> Void) {
+    let context = LAContext()
+    var evaluationError: NSError?
+    let policy: LAPolicy
+    if #available(iOS 9, *) {
+      policy = LAPolicy.deviceOwnerAuthentication
+    } else {
+      policy = LAPolicy.deviceOwnerAuthenticationWithBiometrics
+    }
+    let reason = "We need to check your credentials"
+
+    if (!context.canEvaluatePolicy(policy, error: &evaluationError)) {
+        if(evaluationError?.code == LAError.passcodeNotSet.rawValue) {
+            //TODO: Return exception
+            completionBlock(true)
+        }
+    } else {
+        context.evaluatePolicy(policy, localizedReason: reason) { (success, error) in
+            completionBlock(success)
+        }
+    }
   }
 
 }
